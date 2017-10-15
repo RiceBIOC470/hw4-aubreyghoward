0d940034c8d81bef73511a64c3ae92e42b2f6045
aubreyghoward <aubrey.g.howard@rice.edu>
2017-10-15 14:53:00 -0500

    Q1 Complete

--- cellmaker.m
+++ cellmaker.m
@@ -2,18 +2,14 @@
 % 1024x1024 binary image mask containing 20 circles of that size in random
 % locations
 
-function img = cellmaker(cellsize)
-    img = false(1024)
-    centers = round(rand([20,2])*1024)
-    img(centers) = true;
-    img = imdilate(img,strel('disk',50))
-    
-    img_mask = imgx2 > 0.8
-    
-    img = imdilate(img,strel('disk',cellsize))
-%     imgx2 = img2double(img)
-%     img_mask = imgx2 > 0.8
-%     centers = round(rand([20,2])*1024)
-%     img = imdilate(img,strel('disk',cellsize))
+function img_mask = cellmaker(cellsize)
+    img = false(1024);
+    centers = ceil(rand([20,2])*1024);
+    for ii = 1:length(centers)
+        img(centers(ii,1),centers(ii,2)) = 1;
+    end
+    img_mask = img > 0.5;
+    img_mask = imdilate(img_mask,strel('disk',cellsize));
+
 
 end
